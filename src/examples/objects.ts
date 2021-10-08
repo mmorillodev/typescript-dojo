30f671ecdbf7e17562262426948fa184dff6d61c
Jéssica Rodrigues <jessica.rodrigues@thoughtworks.com>
2021-10-08 14:44:53 -0300

    fix eslint configs

--- src/examples/objects.ts
+++ src/examples/objects.ts
@@ -1,21 +1,21 @@
 // # Objects in JS
 
 const person = {
-  name: 'Iolanda',
+  personName: 'Iolanda',
   age: 45,
   isAdult() {
-    this.age >= 18;
+    return this.age >= 18;
   },
 };
 
 // ES6
 
 class Person {
-  name: string;
+  personName: string;
   age: number;
 
-  constructor(name: string, age: number) {
-    this.name = name;
+  constructor(personName: string, age: number) {
+    this.personName = personName;
     this.age = age;
   }
 
@@ -28,8 +28,8 @@ const person2 = new Person('Iolanda', 45);
 
 // ES5
 
-function PersonOld(this: any, name: string, age: number) {
-  this.name = name;
+function PersonOld(this: any, personName: string, age: number) {
+  this.personName = personName;
   this.age = age;
 
   this.isAdult = function() {
